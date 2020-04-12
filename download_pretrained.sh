df96b0f0f77369196e93852f28510f2015d2c35c
Ivan Panshin <48650449+ivanpanshin@users.noreply.github.com>
2020-04-12 23:17:51 +0500

    Update download_pretrained.sh

--- download_pretrained.sh
+++ download_pretrained.sh
@@ -1,5 +1,25 @@
-kaggle datasets download ivanpan/histopathologic-cancer-detection-weights -f model_efficientnet-b0_fold0.pth -p weights
-kaggle datasets download ivanpan/histopathologic-cancer-detection-weights -f model_efficientnet-b1_fold0.pth -p weights
-unzip weights/model_efficientnet-b0_fold0.pth.zip -d weights
-unzip weights/model_efficientnet-b1_fold0.pth.zip -d weights
+kaggle datasets download ivanpan/histopathologic-cancer-detection-weights -f model_efficientnet-b3_fold0.pth -p weights
+kaggle datasets download ivanpan/histopathologic-cancer-detection-weights -f model_efficientnet-b3_fold1.pth -p weights
+kaggle datasets download ivanpan/histopathologic-cancer-detection-weights -f model_efficientnet-b3_fold2.pth -p weights
+kaggle datasets download ivanpan/histopathologic-cancer-detection-weights -f model_efficientnet-b3_fold3.pth -p weights
+kaggle datasets download ivanpan/histopathologic-cancer-detection-weights -f model_efficientnet-b3_fold4.pth -p weights
+
+kaggle datasets download ivanpan/histopathologic-cancer-detection-weights -f model_seresnet50_fold0.pth -p weights
+kaggle datasets download ivanpan/histopathologic-cancer-detection-weights -f model_seresnet50_fold1.pth -p weights
+kaggle datasets download ivanpan/histopathologic-cancer-detection-weights -f model_seresnet50_fold2.pth -p weights
+kaggle datasets download ivanpan/histopathologic-cancer-detection-weights -f model_seresnet50_fold3.pth -p weights
+kaggle datasets download ivanpan/histopathologic-cancer-detection-weights -f model_seresnet50_fold4.pth -p weights
+
+unzip weights/model_efficientnet-b3_fold0.pth.zip -d weights
+unzip weights/model_efficientnet-b3_fold1.pth.zip -d weights
+unzip weights/model_efficientnet-b3_fold2.pth.zip -d weights
+unzip weights/model_efficientnet-b3_fold3.pth.zip -d weights
+unzip weights/model_efficientnet-b3_fold4.pth.zip -d weights
+
+unzip weights/model_seresnet50_fold0.pth.zip -d weights
+unzip weights/model_seresnet50_fold1.pth.zip -d weights
+unzip weights/model_seresnet50_fold2.pth.zip -d weights
+unzip weights/model_seresnet50_fold3.pth.zip -d weights
+unzip weights/model_seresnet50_fold4.pth.zip -d weights
+
 rm weights/*.zip
